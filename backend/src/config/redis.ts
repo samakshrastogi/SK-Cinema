b4962969914dde514d3db89d34198cea7a6f8ada
Samaksh Rastogi (Nokia) <samaksh.rastogi@nokia.com>
2026-03-19 12:19:45 +0530

    fix redis tls connection

--- backend/src/config/redis.ts
+++ backend/src/config/redis.ts
@@ -1,7 +1,12 @@
 import Redis from "ioredis";
 
-export const redisConnection = new Redis(process.env.REDIS_URL!, {
+if (!process.env.REDIS_URL) {
+    throw new Error("REDIS_URL is not defined ❌");
+}
+
+export const redisConnection = new Redis(process.env.REDIS_URL, {
     maxRetriesPerRequest: null,
+    tls: {}, // ⭐ THIS IS THE FIX
 });
 
 redisConnection.on("connect", () => {
